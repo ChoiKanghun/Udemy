ebe8a6bf2a04b68dd8d49a0fd224b8a53f103f3a
ChoiKanghun <rkdgns4562@naver.com>
2021-05-19 11:02:38 +0900

    카메라 필터 앱 기본 UI 구성

---
+++ CameraFilterRxSwift/CameraFilterRxSwift/ViewControllers/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  CameraFilterRxSwift
+//
+//  Created by 최강훈 on 2021/05/18.
+//
+
+import UIKit
+import RxSwift
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+
+}
+
